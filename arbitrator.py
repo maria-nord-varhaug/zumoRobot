65044f7afdc0e804065a702530a22b4c6c7a175b
Maria Nord-Varhaug <44210425+maria-nord-varhaug@users.noreply.github.com>
2018-10-30 13:14:41 +0100

    Hjelpefiler fra zip

--- arbitrator.py
+++ arbitrator.py
@@ -1,1 +1,9 @@
 class Arbitrator():
+
+    def __init__(self, bbcon):
+        assert isinstance(bbcon, Bbcon)
+        self.bbcon = bbcon
+
+    def choose_action(self):
+        pass
+
---
+++ bbcon.py
@@ -0,0 +1,3 @@
+class Bbcon():
+    def __init__(self):
+        pass
