3c0303b3e041a218bdfe50e3383732f3dccf1f26
Maria Nord-Varhaug <44210425+maria-nord-varhaug@users.noreply.github.com>
2018-10-30 13:31:06 +0100

    definerer metoder og felt

--- arbitrator.py
+++ arbitrator.py
@@ -1,8 +1,13 @@
+from bbcon import Bbcon
+
+#tar imot motorforespørsler fra behaviour, og bestemmer hvilke som skal bli utført :)
+
 class Arbitrator():
 
     def __init__(self, bbcon):
         assert isinstance(bbcon, Bbcon)
         self.bbcon = bbcon
+        self.active_behaviors = self.bbcon.active_behaviors
 
     def choose_action(self):
         pass
--- bbcon.py
+++ bbcon.py
@@ -1,6 +1,25 @@
 class Bbcon():
+
+    #For nå - kun tomme metoder
+
     def __init__(self):
+        self.behaviors = []
+        self.active_behaviors = []
+        self.sensobs = []
+        self.motobs = []
+        self.arbitrator = None
+
+    def add_behavior(self, behavior):
         pass
 
-    def metode(self):
+    def add_sensor(self, sensor):
+        pass
+
+    def activate_behavior(self, behavior):
+        pass
+
+    def deactivate_behavior(self, behavior):
+        pass
+
+    def run_one_timestep(self):
         pass
