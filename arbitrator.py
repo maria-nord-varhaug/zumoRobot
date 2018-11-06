04b79999350d91e53f00803c2ba227e55f4080e3
Maria Nord-Varhaug <44210425+maria-nord-varhaug@users.noreply.github.com>
2018-11-06 16:11:14 +0100

    skriveleif, behavior.weigth istedenfor weight

--- arbitrator.py
+++ arbitrator.py
@@ -5,13 +5,13 @@ from bbcon import Bbcon
 class Arbitrator:
 
     def __init__(self, bbcon):
-        self.bbcon = bbcon
+        self.bbcon = bbcon # tar inn bbcon
 
     def find_optimal_behavior(self):
         largest_weight = 0
         behavior = None  # a behavior object
         for i in range(0, len(self.bbcon.active_behaviors)):                 # for hver behavior i listen i bbcon
-            if self.bbcon.active_behaviors[i].weigth >= largest_weight:     # sammenlign weights
+            if self.bbcon.active_behaviors[i].weight >= largest_weight:     # sammenlign weights
                 behavior = self.bbcon.active_behaviors[i]                       # oppdater viktigste behavior
                 largest_weight = self.bbcon.active_behaviors[i].weight          # oppdater vekt
         return behavior                                                     # returnerer behavior
